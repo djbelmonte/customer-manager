10eaa9c32042b9b696635e7069e3954e022b14df
Donn Jerico Belmonte <belmontedonnjerico@gmail.com>
2025-10-07 17:21:44 +0800

    Add test for customer's name and birthday validation. Add model validation as well to confirm

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,13 @@
 class Customer < ApplicationRecord
+  validates :first_name, :last_name, :birthday, :phone_number, presence: true
+
+  validates :first_name, :last_name, format: { with: /\A[a-zA-Z][a-zA-Z' -]*\z/, message: "Only English letters, hyphen, apostrophe and spaces are allowed." }
+  validate :birthday_should_be_in_the_past
+
+
+  def birthday_should_be_in_the_past
+    if birthday.present? && birthday > Date.today
+      errors.add(:birthday, "Birthday should not be later than today.")
+    end
+  end
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -9,4 +9,50 @@ class CustomerTest < ActiveSupport::TestCase
     assert customer.birthday
     assert customer.phone_number
   end
+  
+  test "customer first_name and last_name validation" do
+    customer = customers(:one)
+
+    valid_names = ["Donn Jerico", "Donn", "Donn-Jerico", "Marc O'neal"]
+    invalid_names = ["Tom & Jerry", "Donn!", "@Donn" "   Donn"]
+
+    valid_names.each do |name|
+      customer.update(first_name: name, last_name: name)
+      customer.reload
+
+      assert_equal name, customer.first_name
+      assert_equal name, customer.last_name
+    end
+    
+    customer.update(first_name: "Customer", last_name: "One")
+
+    invalid_names.each do |name|
+      customer.update(first_name: name, last_name: name)
+      customer.reload
+
+      assert_equal "Customer", customer.first_name
+      assert_equal "One", customer.last_name
+
+      assert_includes customer.errors[:first_name], "Only English letters, hyphen, apostrophe and spaces are allowed."
+      assert_includes customer.errors[:last_name], "Only English letters, hyphen, apostrophe and spaces are allowed."
+    end
+  end
+
+  test "customer birthday validation - customer's birthday can only be from past to present" do
+    customer = customers(:one)
+    current_birthday = customer.birthday
+
+    customer.update(birthday: Date.tomorrow)
+    customer.reload
+    assert_equal current_birthday, customer.birthday
+    assert_includes customer.errors[:birthday], "Birthday should not be later than today."
+
+    customer.update(birthday: Date.yesterday)
+    customer.reload
+    assert_equal Date.yesterday, customer.birthday
+
+    customer.update(birthday: Date.today)
+    customer.reload
+    assert_equal Date.today, customer.birthday
+  end
 end
