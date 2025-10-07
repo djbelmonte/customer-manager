476ccce7df0bab1454fbb5bfc53201db2c3e9749
Donn Jerico Belmonte <belmontedonnjerico@gmail.com>
2025-10-07 16:37:19 +0800

    Add assertions to customer's attributes

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "customer has first name, last name, birthday and phone number" do
+    customer = customers(:one)
+
+    assert customer.first_name
+    assert customer.last_name
+    assert customer.birthday
+    assert customer.phone_number
+  end
 end
